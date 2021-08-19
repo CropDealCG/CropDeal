99cb421f75f1e6e5246b094462dd3b58a93b7175
DarkseidAM <adhish.mathur@gmail.com>
2021-08-19 11:19:00 +0530

    Commit fixed

--- app/src/main/java/com/cg/cropdeal/view/MainActivity.kt
+++ app/src/main/java/com/cg/cropdeal/view/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
 
     }
