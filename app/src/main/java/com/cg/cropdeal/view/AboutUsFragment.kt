d9b34a1adad3b2acd196aed85a9cd0249a224824
Subodh29 <Subodh.chaudhary58@gmail.com>
2021-08-26 09:26:54 +0530

    rate app

--- app/src/main/java/com/cg/cropdeal/view/AboutUsFragment.kt
+++ app/src/main/java/com/cg/cropdeal/view/AboutUsFragment.kt
@@ -1,13 +1,14 @@
 package com.cg.cropdeal.view
 
 import android.app.ActionBar
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.cg.cropdeal.R
 import com.cg.cropdeal.databinding.FragmentAboutUsBinding
 
 
@@ -31,11 +32,23 @@ class AboutUsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //getActionBar()?.setTitle("About Us")
-        binding.rateUSBtn1.setOnClickListener {
+        activity?.actionBar?.setTitle("About Us")
+        binding.rateUSBtn.setOnClickListener {
+            val packageName = "Crop Deals"
+            val uri: Uri = Uri.parse("market://details?id=$packageName")
+            val goToMarket = Intent(Intent.ACTION_VIEW, uri)
 
+            goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or
+                    Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
+                    Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+            try {
+                startActivity(goToMarket)
+            } catch (e: ActivityNotFoundException) {
+                startActivity(Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=$packageName")))
+            }
         }
-        binding.sendFeedbackBtn1.setOnClickListener {
+        binding.sendFeedbackBtn.setOnClickListener {
             val email = Intent(Intent.ACTION_SEND)
 
             val mail={"cropdeals@gmail.com"}.toString()
