6f603aaa22754bf0679f4a9748202b361841885e
DarkseidAM <adhish.mathur@gmail.com>
2021-08-26 18:27:32 +0530

    CropBuyFragment initiated

--- app/src/main/java/com/cg/cropdeal/view/PaymentDetailsFragment.kt
+++ app/src/main/java/com/cg/cropdeal/view/PaymentDetailsFragment.kt
@@ -22,7 +22,7 @@ class PaymentDetailsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentPaymentDetailsBinding.inflate(inflater,container,false)
         return binding.root
     }
