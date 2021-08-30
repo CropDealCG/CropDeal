1ef68b639c7a4a84c0f1e0c83e3f02c89e67dc59
DarkseidAM <adhish.mathur@gmail.com>
2021-08-30 14:10:53 +0530

    Subscriptions for Crops implemented

--- app/src/main/java/com/cg/cropdeal/view/MainActivity.kt
+++ app/src/main/java/com/cg/cropdeal/view/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.cg.cropdeal.view
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -16,26 +19,44 @@ import com.cg.cropdeal.model.Constants
 import com.facebook.FacebookSdk
 import com.google.firebase.database.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
+    private lateinit var subscribedTopic : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         val splashScreen =  installSplashScreen()
         setContentView(binding.root)
-        val subscribedTopic = getSharedPreferences(Constants.TOPIC_PREF,Context.MODE_PRIVATE).getString("topic","")
+
+
+        val sharedPref = getSharedPreferences(Constants.TOPIC_PREF,Context.MODE_PRIVATE)
+        sharedPref.registerOnSharedPreferenceChangeListener(this)
+        subscribedTopic = sharedPref.getString("topic","")!!
         if(!subscribedTopic.isNullOrEmpty()){
             FirebaseDatabase.getInstance().reference.child("crops").addChildEventListener(object : ChildEventListener{
                 override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
                     if(snapshot.exists()){
-                        var notificationBuilder : NotificationCompat.Builder
-                        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
-                            val channel = NotificationChannel("Subscriptions","notification",NotificationManager.IMPORTANCE_DEFAULT)
-                            notificationBuilder = NotificationCompat.Builder(applicationContext,"Subscriptions")
+                        val nManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                        val builder: Notification.Builder = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {//checks version
+                            val channel = NotificationChannel("Subscriptions", "Work Done", NotificationManager.IMPORTANCE_DEFAULT)
+                            nManager.createNotificationChannel(channel)
+                            Notification.Builder(applicationContext, "Subscriptions")
+                        } else  Notification.Builder(applicationContext)
+                        builder.setSmallIcon(R.drawable.logo_without_text)
+                        builder.setContentTitle("$subscribedTopic is available!")
+                        builder.setContentText("Buy NOW")
+                        val notifyIntent = Intent(this@MainActivity,NavigationActivity::class.java).apply {
+                            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                         }
-                        else{
-                            notificationBuilder = NotificationCompat.Builder(applicationContext,"Subscriptions")
+                        val notifyPendingIntent = PendingIntent.getActivity(
+                            applicationContext, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT
+                        )
+                        builder.setContentIntent(notifyPendingIntent)
+                        builder.setAutoCancel(true)
+                        val myNotify =builder.build()
+                        if(snapshot.child("cropName").value.toString() == subscribedTopic){
+                            nManager.notify(99,myNotify)
                         }
-                        Toast.makeText(applicationContext,"Database changed",Toast.LENGTH_LONG).show()
+
                     }
                 }
 
@@ -66,6 +87,10 @@ class MainActivity : AppCompatActivity() {
         FacebookSdk.sdkInitialize(this)
         startActivity(Intent(this,SignUpActivity::class.java))
         finish()
+    }
+
+    override fun onSharedPreferenceChanged(p0: SharedPreferences?, p1: String?) {
+        subscribedTopic = p0?.getString("topic","")!!
     }
 
 }
