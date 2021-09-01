e6d6228a13bad31353f4869cb1500a8f9b2c7964
Subodh29 <Subodh.chaudhary58@gmail.com>
2021-09-01 09:00:37 +0530

    admin reports intialised

--- app/src/main/java/com/cg/cropdeal/view/AdminActivity.kt
+++ app/src/main/java/com/cg/cropdeal/view/AdminActivity.kt
@@ -2,11 +2,18 @@ package com.cg.cropdeal.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
 import com.cg.cropdeal.databinding.AdminMainActivityBinding
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.auth.FirebaseAuth
+import android.widget.Toast
+import com.cg.cropdeal.R
 
+
+const val INVOICES = "Invoices"
+        const val CROPS = "Crops"
 class AdminActivity : AppCompatActivity() {
+
     private lateinit var binding : AdminMainActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +21,29 @@ class AdminActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         FirebaseAuth.getInstance()
+
+        binding.reportManagementTV.setOnClickListener {
+            val popup = PopupMenu(this, it)
+
+            popup.menu.add(INVOICES)
+            popup.menu.add(CROPS)
+            popup.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item ->
+                when(item.title){
+                    INVOICES -> {
+                        supportFragmentManager.beginTransaction()
+                            .add(R.id.container,AdminInvoiceFragment(),
+                                "AdminInvoiceFragment")
+                            .addToBackStack(null)
+                            .commit()
+                    }
+                    CROPS -> {}
+
+                }
+
+                true
+            })
+
+            popup.show()
+        }
     }
 }
---
+++ app/src/main/java/com/cg/cropdeal/viewmodel/AdminReportVM.kt
@@ -0,0 +1,44 @@
+package com.cg.cropdeal.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.cg.cropdeal.model.Invoice
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class AdminReportVM(application: Application):AndroidViewModel(application) {
+    private var invoiceList : MutableLiveData<List<Invoice>>? = null
+    private var currentInvoiceList : MutableList<Invoice>? = null
+    private var firebaseDatabase : FirebaseDatabase? = null
+
+
+    init {
+        invoiceList = MutableLiveData()
+        currentInvoiceList = mutableListOf()
+        firebaseDatabase = FirebaseDatabase.getInstance()
+    }
+
+    private fun populateInvoiceList(uid: String?) {
+        firebaseDatabase?.reference?.child("invoice")?.addValueEventListener(object :
+            ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                currentInvoiceList?.clear()
+                for(child in snapshot.children){
+                    val invoice = child.getValue(Invoice::class.java)
+                    currentInvoiceList?.add(invoice!!)
+                    invoiceList?.value = currentInvoiceList
+                }
+                invoiceList?.value = currentInvoiceList
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+
+        })
+    }
+    fun getInvoice() : MutableLiveData<List<Invoice>>? = invoiceList
+
+}
